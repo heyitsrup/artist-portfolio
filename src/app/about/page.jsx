f13b4634890d75df34cd8f0eeb7936872fc593cc
Rup Singh <rs02002@surrey.ac.uk>
2025-02-14 19:14:01 +0000

    about page mobile view

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,5 +1,3 @@
-// src/app/pages/about.jsx
-
 import React from 'react';
 import Image from 'next/image';
 import EnquireForm from '../components/EnquireForm.jsx';
@@ -7,12 +5,14 @@ import EnquireForm from '../components/EnquireForm.jsx';
 export default function About() {
   return (
     <div>
-      <p className="font-serif text-center text-[2vw] font-light py-4">
+      <p className="font-serif text-center md:text-[2vw] font-light py-4">
         PASSIONATE ABOUT BRINGING DREAMS ON TO A CANVAS
       </p>
 
-      <div className="flex items-center px-4 sm:px-8 md:px-16 lg:px-64">
-        <div className="bg-transparent flex-shrink-0 w-1/2">
+      {/* Adjust flex direction for small screens */}
+      <div className="flex flex-col md:flex-row items-center px-4 sm:px-8 md:px-16 lg:px-64">
+        {/* Image Section */}
+        <div className="bg-transparent flex-shrink-0 w-full md:w-1/2">
           <Image
             src="/images/Selfie.JPG"
             alt="profile"
@@ -22,7 +22,8 @@ export default function About() {
           />
         </div>
 
-        <div className="bg-transparent px-4 sm:px-8 w-full sm:w-1/2">
+        {/* Text Section */}
+        <div className="bg-transparent px-4 sm:px-8 w-full md:w-1/2 text-center md:text-left">
           <p className="font-serif text-2xl pb-2">Hi there!</p>
           <p className="font-serif text-xl py-2">
             I'm Lawanya, an artist who’s been drawing and painting since childhood.
@@ -30,18 +31,18 @@ export default function About() {
             Each piece allows me to explore and share the depth of feeling and story behind a simple moment.
             I’m excited to invite you into this emotional, artistic journey!
           </p>
-          <p className="text-[1.5vw] tracking-[0.25em] font-bold text-green-700 pt-2">
+          <p className="md:text-[1.5vw] tracking-[0.25em] font-bold text-green-700 pt-2">
             enquire below
           </p>
         </div>
       </div>
 
+      {/* Enquiry Form Section */}
       <div className="flex items-center justify-center py-2 px-4 sm:px-8">
         <div className="max-w-screen-md w-full">
           <EnquireForm />
         </div>
       </div>
-
     </div>
   );
-};
+}
